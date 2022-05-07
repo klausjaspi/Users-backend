adc23b01fddf62d2c65eacd175761c5fc3d1153b
klausjaspi <klaus.jaspi@gmail.com>
2022-05-07 16:21:47 +0300

    Added App user and registration Database setup

--- src/main/java/com/example/demo/appuser/AppUser.java
+++ src/main/java/com/example/demo/appuser/AppUser.java
@@ -1,42 +1,86 @@
 package com.example.demo.appuser;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.*;
+import java.util.Collections;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity
 public class AppUser implements UserDetails {
+
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    private Long id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private AppUserRole appUserRole;
+    private Boolean locked;
+    private Boolean enabled;
+
+    public AppUser(String name, String username, String email, String password, AppUserRole appUserRole, Boolean locked, Boolean enabled) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.appUserRole = appUserRole;
+        this.locked = locked;
+        this.enabled = enabled;
+    }
 
     @java.lang.Override
     public java.util.Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        SimpleGrantedAuthority auth = new SimpleGrantedAuthority(appUserRole.name());
+        return Collections.singletonList(auth);
     }
 
     @java.lang.Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @java.lang.Override
     public java.lang.String getUsername() {
-        return null;
+        return username;
     }
 
     @java.lang.Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @java.lang.Override
     public boolean isAccountNonLocked() {
-        return false;
+        return !locked;
     }
 
     @java.lang.Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @java.lang.Override
     public boolean isEnabled() {
-        return false;
+        return enabled;
     }
 }
