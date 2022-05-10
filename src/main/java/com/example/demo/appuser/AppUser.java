e4d54915669b51c27d412e48f64f25cfed00a52a
klausjaspi <klaus.jaspi@gmail.com>
2022-05-11 00:31:06 +0300

    User registration & confirmation token

--- src/main/java/com/example/demo/appuser/AppUser.java
+++ src/main/java/com/example/demo/appuser/AppUser.java
@@ -29,23 +29,21 @@ public class AppUser implements UserDetails {
             generator = "student_sequence"
     )
     private Long id;
-    private String name;
-    private String username;
+    private String firstName;
+    private String lastName;
     private String email;
     private String password;
     @Enumerated(EnumType.STRING)
     private AppUserRole appUserRole;
-    private Boolean locked;
-    private Boolean enabled;
+    private Boolean locked = false;
+    private Boolean enabled = false;
 
-    public AppUser(String name, String username, String email, String password, AppUserRole appUserRole, Boolean locked, Boolean enabled) {
-        this.name = name;
-        this.username = username;
+    public AppUser(String firstName, String lastName, String email, String password, AppUserRole appUserRole) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
         this.appUserRole = appUserRole;
-        this.locked = locked;
-        this.enabled = enabled;
     }
 
     @java.lang.Override
@@ -54,14 +52,22 @@ public class AppUser implements UserDetails {
         return Collections.singletonList(auth);
     }
 
-    @java.lang.Override
+    @Override
     public String getPassword() {
         return password;
     }
 
-    @java.lang.Override
-    public java.lang.String getUsername() {
-        return username;
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 
     @java.lang.Override
